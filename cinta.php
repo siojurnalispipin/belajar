99dda3730067a5dfdc8c153e7c2c98fc79a18853
siojurnalispipin <siojurnalispipin@hotmail.com>
2017-11-24 00:43:35 +0700

    reset

--- cinta.php
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-inilah awal sejarah hehe
-ubah file cinta
-
-ini cinta yang hilang dari medan
-=======
-inilah awal sejarah hehe
->>>>>>> parent of 67943a5... aku masukkan pesan disini
